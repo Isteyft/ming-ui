5ae4984b5616b8f88802337ba24a5c0379815d02
Isteyft <14056025+isteyft@user.noreply.gitee.com>
2025-07-30 17:24:30 +0800

    docs修复

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import type { App } from "vue";
-// import { ElementPlusContainer } from "vitepress-preview-component";
+import { ElementPlusContainer } from "@vitepress-demo-preview/component";
 import DefaultTheme from "vitepress/theme";
 import BaizeUI from "baize-ui";
 
@@ -9,7 +9,7 @@ import "baize-ui/dist/index.css";
 export default {
   ...DefaultTheme,
   enhanceApp({ app }: { app: App }) {
-    // app.component("demo-preview", ElementPlusContainer);
+    app.component("demo-preview", ElementPlusContainer);
     app.use(BaizeUI);
   },
 };
